3ffbe54b077efe50571f84c86373d04108f3ae71
Владимир <bezrukovq@yandex.ru>
2018-07-12 12:53:50 +0300

    design+sound on tip

--- app/src/main/java/mm/memeonare/DataC/Question.java
+++ app/src/main/java/mm/memeonare/DataC/Question.java
@@ -3,7 +3,27 @@ package mm.memeonare.DataC;
 import java.util.ArrayList;
 
 public class Question {
+    private String mem;
     private String question;
-    private ArrayList<String> answers= new ArrayList<>();
+    private ArrayList<String> answers;
+    private int cIndex;
 
+    public Question(String mem, String question, ArrayList<String> answers, int cIndex) {
+        this.mem = mem;
+        this.question = question;
+        this.answers = answers;
+        this.cIndex = cIndex;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public ArrayList<String> getAnswers() {
+        return answers;
+    }
+
+    public int getcIndex() {
+        return cIndex;
+    }
 }
--- app/src/main/java/mm/memeonare/GameActivity.java
+++ app/src/main/java/mm/memeonare/GameActivity.java
@@ -1,24 +1,54 @@
 package mm.memeonare;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class GameActivity extends AppCompatActivity {
-    Button Button1;
-    ImageView mem;
-    Button backButton;
-    Button bTip;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+import mm.memeonare.DataC.Question;
+
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private ImageView mem;
+    private Button backButton;
+    private Button bTip;
+    private TextView question;
+    private ArrayList<Question> questions= new ArrayList<>();
+
+
+
+    private int r =0;
+    private int correctButton =2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        mem=findViewById(R.id.mem);
-        bTip=findViewById(R.id.bTip);
-        backButton=findViewById(R.id.backButton);
+
+        getQuestions();
+        question=findViewById(R.id.question);
+        mem = findViewById(R.id.mem);
+        bTip = findViewById(R.id.bTip);
+        backButton = findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,13 +57,77 @@ public class GameActivity extends AppCompatActivity {
                 finish();
             }
         });
-        Button1=findViewById(R.id.button1);
-        Button1.setOnClickListener(new View.OnClickListener() {
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+        button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+        button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+
+        bTip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mem.setImageResource(R.drawable.button);
-                mem.invalidate();
+                new Thread(){
+                    public void run(){
+                        MediaPlayer mp = MediaPlayer.create(GameActivity.this, R.raw.natalyamp);
+                        mp.start();
+                    }
+                }.start();
             }
         });
+    }
+
+    @Override
+    public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+        return super.onCreateView(parent, name, context, attrs);
+    }
+
+    int translateIdToIndex(int id) {
+        int index = -1;
+        switch (id) {
+            case R.id.button1:
+                index = 1;
+                break;
+            case R.id.button2:
+                index = 2;
+                break;
+            case R.id.button3:
+                index = 3;
+                break;
+            case R.id.button4:
+                index = 4;
+                break;
+        }
+        return index;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast toast;
+        int buttonIndex = translateIdToIndex(view.getId());
+        if (buttonIndex == correctButton) {
+            toast = Toast.makeText(getApplicationContext(), "CORRECT", Toast.LENGTH_SHORT);
+            mem.setImageResource(R.drawable.button1);
+            correctButton= (int)(1+Math.random()*4);
+            question.setText(Integer.toString(r++));
+        } else {
+            toast = Toast.makeText(getApplicationContext(), "INCORRECT", Toast.LENGTH_SHORT);
+        }
+        toast.show();
+        mem.setImageResource(R.drawable.natalya);
+
+    }
+
+    public void getQuestions(){
+        try {
+            Scanner sc = new Scanner(new File("questions.txt"));
+            sc.nextLine();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }

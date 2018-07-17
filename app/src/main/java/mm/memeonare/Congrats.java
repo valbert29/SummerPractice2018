95979cc038d61fc31f996bcd970b9fa019898fd4
Владимир <bezrukovq@yandex.ru>
2018-07-17 11:44:21 +0300

    игеещт

---
+++ app/src/main/java/mm/memeonare/Congrats.java
@@ -0,0 +1,55 @@
+package mm.memeonare;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class Congrats extends DialogFragment {
+    public class DialogF extends DialogFragment {
+        MyListener callback;
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+            super.onCreateDialog(savedInstanceState);
+            View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialogf_lay,null);
+            AlertDialog.Builder adb = new AlertDialog.Builder(getActivity());
+            adb.setTitle("").set
+                    .setView(view)
+                    .setPositiveButton("GO TO MENU", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            callback.callback();
+                        }
+                    });
+
+            return adb.create();
+        }
+
+
+        @Override
+        public void onAttach(Context context) {
+            super.onAttach(context);
+            if (context instanceof MyListener) {
+                callback = (MyListener) context;
+            } else {
+                throw new RuntimeException(context.toString()
+                        + " must implement MyListener");
+            }
+        }
+
+        @Override
+        public void onDetach() {
+            super.onDetach();
+            callback = null;
+        }
+    }
+
+}

939db32be50a1522cd9615c266c6962a8ea58856
lion <kotleta.first@gmail.com>
2018-07-17 11:51:19 +0300

    fixes

--- app/src/main/java/mm/memeonare/GameActivity.java
+++ app/src/main/java/mm/memeonare/GameActivity.java
@@ -136,6 +136,9 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         buttonIndex = translateIdToIndex(view.getId());
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
                 WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+        for (Button b : buttons) {
+            b.setEnabled(false);
+        }
        /* view.invalidate();
         try {
             Thread.sleep(3000);
@@ -155,7 +158,6 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         if (buttonIndex == correctButton) {
             doublechance=false;
             if (qnum != 10) {
-
                 nextquestion();
             } else finishgame();
         } else {
@@ -199,6 +201,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         for (Button b : buttons) {
             b.setBackground(getDrawable(R.drawable.button));
             b.setClickable(true);
+            b.setEnabled(true);
         }
         tip50Pressed = false;
         Question question = (Question) questions.get(qnum).get((int) (Math.random() * questions.get(qnum).size()));
